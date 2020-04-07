2db8d473d8adba0d2623bc6216b6fd42b668dc6f
Daxa <apelsinKDA@gmail.com>
2020-04-07 20:53:57 +0700

    created a simple registration + little changes in main.py

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired
 
 
 class RegisterForm(FlaskForm):
-    nickname = StringField('Логин', validators=[DataRequired])
+    nickname = StringField('Логин', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
     submit = SubmitField('Зарегистрироваться')
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from load_palletes_from_lospec import load_palettes
 from posts import load_posts
 
-from flask import make_response, jsonify, render_template
+from flask import render_template, redirect
+from flask_login import login_required, logout_user, login_user
 
 from app import app, get_db_session, login_manager
 from app.models import Post, User
@@ -21,16 +22,47 @@ def index():
     return render_template('all_posts.html', title='Главная', posts=posts)
 
 
-@app.route('/login')
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html', title='Главная', form=form)
+    if form.validate_on_submit():
+        session = get_db_session()
+        user = session.query(User).filter(User.nickname == form.nickname.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html', title='Вход',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Вход', form=form)
 
 
-@app.route('/registration')
+@app.route('/registration', methods=['GET', 'POST'])
 def registration():
     form = RegisterForm()
-    return render_template('registration.html', title='Главная', form=form)
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('registration.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        session = get_db_session()
+        if session.query(User).filter(User.nickname == form.nickname.data).first():
+            return render_template('registration.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(nickname=form.nickname.data)
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        return redirect('/login')
+    return render_template('registration.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':
