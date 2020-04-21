9b70ba5b4e8849ed77c7752a1fc0e43c101b54b4
Daxa <apelsinKDA@gmail.com>
2020-04-21 21:39:52 +0700

    little changes in pages' looking

--- app/static/js/post.js
+++ app/static/js/post.js
@@ -6,7 +6,11 @@ window.onload = getData();
 
 let reader = new FileReader();
 let imgInput = $("#image");
+let button = $('#post');
 imgInput.change(function (event) {
+    button.text('Дождитесь окончания загрузки');
+    button.prop('disabled', true);
+
     let file = event.target.files[0];
     reader.readAsDataURL(file);
     reader.onload = function () {
@@ -14,14 +18,16 @@ imgInput.change(function (event) {
             url: '/api/posts/picture',
             type: 'POST',
             data: {'image': reader.result},
-            async: false,
             success: function (data) {
                 attachment = data.attachment
             },
             fail: function () {
             }
+        }).always(function () {
+            button.text('Отправить');
+            button.prop('disabled', false);
         })
-    }
+    };
 });
 
 
@@ -31,46 +37,37 @@ function createPost() {
     let fromGroup = $('#fromGroup').is(':checked');
     let signed = $('#signed').is(':checked');
     let unix = 0;
-    if ( message !== '' || attachment !== null ) {
+    if (message !== '' || attachment !== null) {
         $('#contPost').remove('.alert');
         if (datetime.length !== 0) {
             unix = getUnix(-1 * (new Date().getTimezoneOffset() / 60));
         }
-        if (unix === false) {
+        if (unix < Number((new Date().getTime()).toString().slice(0, 10))) {
             if ($('.alert').length === 0) {
                 createError('Неверный формат ввода даты/времени публикации');
-                return
             }
+            return
         }
-        console.log(unix);
+        let params = {
+            owner_id: Number(ownerId),
+            message: message,
+            publish_date: unix,
+            attachments: attachment,
+            from_group: Number(fromGroup),
+            signed: Number(signed),
+            v: "5.103"
+        };
         if (postId !== null) {
-            VK.Api.call('wall.post', {
-                    owner_id: Number(ownerId),
-                    message: message,
-                    publish_date: unix,
-                    attachments: attachment,
-                    from_group: Number(fromGroup),
-                    signed: Number(signed),
-                    post_id: postId,
-                    v: "5.103"
-                },
-                function () {
-                    window.location.href = '/'
-                })
-        } else {
-            VK.Api.call('wall.post', {
-                    owner_id: Number(ownerId),
-                    message: message,
-                    publish_date: unix,
-                    attachments: attachment,
-                    from_group: Number(fromGroup),
-                    signed: Number(signed),
-                    v: "5.103"
-                },
-                function () {
-                    window.location.href = '/'
-                })
+            params.post_id = postId;
         }
+        VK.Api.call('wall.post', params,
+            function (resp) {
+            console.log(resp.error);
+                if (resp.error === undefined){
+                    window.location.href = '/'
+                }
+            })
+
     } else {
         if ($('.alert').length === 0) {
             createError('Пустой пост опубликовать невозможно')
@@ -84,10 +81,10 @@ function createError(message) {
     error.classList.add('alert');
     error.classList.add('alert-secondary');
     error.setAttribute('role', 'alert');
-    error.setAttribute('style', 'width: 100%; align-content: center');
+    error.setAttribute('style', 'width: 100%; margin-top: 1vh');
     error = $(error);
     error.text(message);
-    $('.row').append(error)
+    $('#contPost').append(error)
 }
 
 
@@ -96,7 +93,6 @@ function getUnix(tz) {
     let date = datetime[0];
     let time = datetime[1];
     let unix = 0;
-    console.log(tz.toString());
     $.ajax({
         url: `/api/posts/unixtime_${date}_${time}_${tz.toString()}`,
         type: 'GET',
@@ -106,7 +102,7 @@ function getUnix(tz) {
             unix = data.unixtime
         },
     }).fail(function () {
-        unix = false
+        unix = 0
     });
     return unix
 }
@@ -115,9 +111,9 @@ function getData() {
     if (window.location.hash !== '') {
         $('#contPost').removeClass('row');
         $('#image').remove();
-        $('#fromGroup').remove();
+        $('#fromGroup').attr("checked", "checked");
+        $('#fromGroup').prop('disabled', true);
         $('#signed').attr("checked", "checked");
-        $('.s-button ').text('В очередь');
 
         postId = window.location.hash.slice(1);
         VK.Api.call('wall.getById', {
@@ -125,6 +121,12 @@ function getData() {
             v: "5.103"
         }, function (data) {
             $("#description").text(data.response[0].text);
+            if (data.response[0].attachments.length > 1) {
+                createError('ВНИМАНИЕ. ' +
+                    'Количество медиа-объектов этого поста больше одной фотографии. ' +
+                    'Рекомендуем Вам продолжить подготовку публикации в Вашей группе ВКонтакте, ' +
+                    'чтобы избежать потери данных.')
+            }
 
             let image = document.createElement('img');
             image.setAttribute('src', data.response[0].attachments[0].photo.sizes[3].url);
